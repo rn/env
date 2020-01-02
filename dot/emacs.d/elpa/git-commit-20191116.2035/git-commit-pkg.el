d2d1be7041760381e723a583fadd813b12b5cc32
Rolf Neugebauer <rn@rneugeba.io>
2020-01-02 21:48:52 +0000

    dot: Update emacs packages to latest
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- dot/emacs.d/elpa/git-commit-20191116.2035/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20191116.2035" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "18d4e7eaf18de965b57719c3d49317d182939749" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- dot/emacs.d/elpa/transient-20190928.1749/transient-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; transient-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("transient-pkg.el" "transient.el") (23952
-;;;;;;  41519 764681 831000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; transient-autoloads.el ends here
