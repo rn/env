35db2c33ee5399daa1ed635230d2f071036d7f80
Rolf Neugebauer <rn@rneugeba.io>
2020-05-14 10:03:42 +0100

    dot: Update Emacs files
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- dot/emacs.d/elpa/memoize-20191126.2002/memoize-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; memoize-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("memoize.el") (24057 15203 656487 663000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; memoize-autoloads.el ends here
