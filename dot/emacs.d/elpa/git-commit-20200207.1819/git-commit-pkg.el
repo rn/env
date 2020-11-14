b8587619fb7f175e4b1e334b6193f43e28e0ae49
Rolf Neugebauer <rn@rneugeba.io>
2020-11-14 15:05:30 +0000

    dot: Update emacs packages
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- dot/emacs.d/elpa/git-commit-20200207.1819/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200207.1819" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "78e37d0d01c9c109459877d6b13e2e5c308fb14e" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
