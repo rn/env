83991f819fc2eb1bf029a77c36bc45609cce9a57
Rolf Neugebauer <rn@rneugeba.io>
2021-02-13 18:08:04 +0000

    dot: Update emacs packages
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

---
+++ dot/emacs.d/elpa/dtrt-indent-20210213.1323/dtrt-indent-pkg.el
@@ -0,0 +1,9 @@
+(define-package "dtrt-indent" "20210213.1323" "Adapt to foreign indentation offsets" 'nil :commit "d39436cc2dcc95a54d78c94a3af326d1fd6f6cae" :authors
+  '(("Julian Scheid" . "julians37@googlemail.com"))
+  :maintainer
+  '("Reuben Thomas" . "rrt@sc3d.org")
+  :keywords
+  '("convenience" "files" "languages" "c"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
