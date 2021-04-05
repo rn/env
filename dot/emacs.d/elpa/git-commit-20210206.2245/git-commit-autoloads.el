5cba08d373b634e458c88638b58c056a3d6f46c4
Rolf Neugebauer <rn@rneugeba.io>
2021-04-05 13:58:33 +0100

    emacs: Update packages
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- dot/emacs.d/elpa/git-commit-20210206.2245/git-commit-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; git-commit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("git-commit.el") (24616 5464 144328 102000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; git-commit-autoloads.el ends here
