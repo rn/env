c0e3c83bdb0080a5f371a4585eebdc134522d288
Rolf Neugebauer <rn@rneugeba.io>
2023-12-02 18:49:25 +0000

    emacs: Update packages
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- dot/emacs.d/elpa/ggtags-20211008.528/ggtags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ggtags" "20211008.528" "emacs frontend to GNU Global source code tagging system" '((emacs "25")) :commit "1442ab8c7f02d246f14150207534fba4a42201ba" :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com") :keywords '("tools" "convenience") :url "https://github.com/leoliu/ggtags")
--- dot/emacs.d/elpa/magit-section-20211004.1955/magit-section-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; magit-section-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("magit-section-pkg.el" "magit-section.el")
-;;;;;;  (24929 36747 774580 220000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-section-autoloads.el ends here
