f21efa1afb1d69b119e9bbcd053521647ae58d16
Rolf Neugebauer <rn@rneugeba.io>
2024-11-19 21:01:42 +0000

    backup-nas2nas: Local NAS backup script
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

---
+++ tools/bin/backup-nas2nas.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# Backup NAS home directory to the NAS restic volume. To be executed on the NAS
+
+cd /share/rn
+export TMPDIR=~/tmp/restic
+
+restic backup -v -r /share/backup-restic/rn --exclude="*.lrprev" --exclude="dovecot.index.cache" docs photos music work .Maildir email-archive 
