11b130658cf1a7c34ed0ea208def9fcc899b7259
Rolf Neugebauer <rn@rneugeba.io>
2026-02-15 15:01:19 +0000

    emacs: Update packages
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

---
+++ dot/emacs.d/elpa/flycheck-20260215.1026/flycheck-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "flycheck" "20260215.1026"
+  "On-the-fly syntax checking."
+  '((emacs "27.1")
+    (seq   "2.24"))
+  :url "https://www.flycheck.org"
+  :commit "44bed1fbc48a76f6c3a9fb9021f6c4d92fb14577"
+  :revdesc "44bed1fbc48a"
+  :keywords '("convenience" "languages" "tools")
+  :authors '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
+  :maintainers '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
+                 ("fmdkdd" . "fmdkdd@gmail.com")
+                 ("Bozhidar Batsov" . "bozhidar@batsov.dev")))
